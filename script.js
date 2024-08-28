10248be014dc17a2b97e81f8ed43460af3383829
Francisco Sabino <fdasabino@gmail.com>
2024-08-28 12:43:10 +0200

    Update index.html to include script.js for additional functionality

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello, class!");
+const themeButton = document.querySelector("button");
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-theme");
+}
+
+themeButton.addEventListener("click", toggleTheme);
