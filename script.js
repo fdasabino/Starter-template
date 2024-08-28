980dd8817c05ab02a4b36b492e32214dc66c774c
Francisco Sabino <fdasabino@gmail.com>
2024-08-28 12:58:53 +0200

    Update index.html and styles.css for CSS Variables Inheritance

--- script.js
+++
@@ -1,7 +0,0 @@
-const themeButton = document.querySelector("button");
-
-function toggleTheme() {
-  document.body.classList.toggle("dark-theme");
-}
-
-themeButton.addEventListener("click", toggleTheme);
