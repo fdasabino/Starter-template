8e69a82aea5dbcd2dd7bb69e6c0fa95066364428
Francisco Sabino <fdasabino@gmail.com>
2024-08-30 09:02:00 +0200

    Refactor HTML structure and update page title

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+"use strict";
+
+// Elements
 const nav = document.querySelector("nav");
 const openBtn = document.querySelector(".menu-button");
 const mainContent = document.querySelector("main");
+const navLinks = document.querySelectorAll("nav ul li a");
 
+// Functions
 function toggleNav() {
   nav.classList.toggle("show");
 }
 
-function closeNav() {
+function closeNav(e) {
+  e.preventDefault();
   nav.classList.remove("show");
 }
 
+// Event Listeners
 openBtn.addEventListener("click", toggleNav);
 mainContent.addEventListener("click", closeNav);
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeNav);
+});
